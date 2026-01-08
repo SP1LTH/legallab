a0df70ea0df5b68de650ceb00a2c6ce847551645
SP1LTH <noob.gamer772onroblox@gmail.com>
2026-01-08 12:26:35 +0800

    patch

--- backend/models/Dictionary.js
+++ backend/models/Dictionary.js
@@ -1,16 +1,1 @@
-// © 2025 Altangerel Ganbaatar. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for details.
-
-const mongoose = require('mongoose');
-
-const dictionarySchema = new mongoose.Schema({
-  term: { type: String, required: true },
-  explanation: { type: String, required: true },
-  extra: { type: String },
-  ru: { type: String },
-  en: { type: String },
-  la: { type: String },
-  de: { type: String },
-});
-
-module.exports = mongoose.model('Dictionary', dictionarySchema);
+.
